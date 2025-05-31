350727498e103a648c30cfe560418b539da6c9b1
-kikukick- <k5agentkr@gmail.com>
2025-05-31 13:25:40 +0900

    Update account.js

--- pages/my_account/account.js
+++ pages/my_account/account.js
@@ -1,6 +1,10 @@
-document.getElementById("userIcon").addEventListener("click", function() {
-  const move = confirm("移動しますか？");
-  if (move) {
-    window.location.href = "https://kikukick.github.io/kikukick_program_page/";
+document.addEventListener("DOMContentLoaded", function () {
+  const userIcon = document.getElementById("userIcon");
+  if (userIcon) {
+    userIcon.addEventListener("click", function () {
+      if (confirm("移動しますか？")) {
+        window.location.href = "https://kikukick.github.io/kikukick_program_page/";
+      }
+    });
   }
 });
