48e28d011c7dddd14dedd8d3cb8091b448593459
-kikukick- <k5agentkr@gmail.com>
2025-05-31 13:04:10 +0900

    Create account.js

---
+++ pages/my account/account.js
@@ -0,0 +1,5 @@
+document.getElementById("userIcon").addEventListener("click", function() {
+  if (confirm("移動しますか？")) {
+    window.location.href = "https://kikukick.github.io/kikukick_program_page/"; // ホームページURL
+  }
+});
