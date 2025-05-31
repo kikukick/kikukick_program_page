1bd9840dd78ba4243449bc6328bee5eddce36c77
-kikukick- <k5agentkr@gmail.com>
2025-05-31 13:12:16 +0900

    Delete pages/my account directory

--- pages/my account/account.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("userIcon").addEventListener("click", function() {
-  const move = confirm("移動しますか？");
-  if (move) {
-    window.location.href = "https://kikukick.github.io/kikukick_program_page/";
-  }
-});
